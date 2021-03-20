4fabd7313f8c2006b3cf7aa5ceebeab4301d221e
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-03-20 11:08:48 +0000

    Add files via upload

--- celsius-to-fahrenheit.js
+++ celsius-to-fahrenheit.js
@@ -1,17 +1,28 @@
 var cEl = document.getElementById("cel");
-var fEl = document.getElementById("fahResult");
-
+var fEl = document.getElementById("fah");
 function convertC() {
     var celsius = cEl.value * 9 / 5 + 32;
-//    fEl.value = celsius.toFixed(4);
-    fEl.value = celsius;
+	function isDecimal(num) {
+		var result = (num - Math.floor(num)) !== 0;
+		if (result)
+			fEl.value = celsius.toFixed(1);
+		else
+			fEl.value = celsius;
+	}
+	isDecimal(celsius);
 	console.log(cEl.value);
 	console.log(fEl.value);
 }
 function convertF() {
 	var fahrenheit = (fEl.value - 32) * 5 / 9;
-//	cEl.value = fahrenheit.toFixed(4);
-    cEl.value = fahrenheit;
+	function isDecimal(num) {
+		var result = (num - Math.floor(num)) !== 0;
+		if (result)
+			cEl.value = fahrenheit.toFixed(1);
+		 else
+			 cEl.value = fahrenheit;
+	}
+	isDecimal(fahrenheit);
 	console.log(fEl.value);
 	console.log(cEl.value);
 }
